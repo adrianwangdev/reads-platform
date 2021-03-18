a2a2709d5fdd7e9cfa1d869022c659c7ac1f741d
= <=>
2021-03-19 00:11:26 +0800

    feat: build write page (CkEditor, render posts from firebase db)

--- src/views/Write/index.js
+++ src/views/Write/index.js
@@ -3,7 +3,8 @@ import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { actions } from './stores'
 import { Grid } from '@material-ui/core'
-
+import { useWatch } from 'utilities/useWatch'
+import { dateGenerator } from 'utilities/unixTimestampConverter'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
@@ -11,11 +12,19 @@ import {
   WritePage,
   WriteWrapper,
   GridWhiteBackground,
-  ArticleTitleInput
-} from "./write.styled";
+  ArticleTitleInput,
+  SubmitPostButton,
+  SectionTitle,
+  PostList,
+  PostItem,
+  PostTitle,
+  PostContent,
+  CreatedTime,
+} from "./write.styled"
 
 const Write = ({
   userLogin,
+  keepUserLogin,
   postList,
   articleTitle,
   articleContent,
@@ -27,13 +36,25 @@ const Write = ({
 
   const titleRef = useRef()
 
-  useEffect(getMyPostList, [postList, getMyPostList])
+  const renderMyPostList = list => {
+    const sortedList = list.sort((next, prev) => prev.createTime - next.createTime)
+    return (
+      sortedList.map(item => (
+        <PostItem key={item.createTime}>
+          <PostTitle>{item.title}</PostTitle>
+          <PostContent dangerouslySetInnerHTML={{__html: item.content}}/>
+          <CreatedTime>{dateGenerator(item.createTime)}</CreatedTime>
+        </PostItem>
+      ))
+    )
+  }
+  useWatch(keepUserLogin)
+  useEffect(getMyPostList, [getMyPostList])
 
   return (
-    // !userLogin
-    //   ? <Redirect to='/login' />
-    //   : ()
-
+    !userLogin
+      ? <Redirect to='/login' />
+      : (
         <WritePage>
           <WriteWrapper maxWidth="md">
             <Grid container spacing={4}>
@@ -43,40 +64,29 @@ const Write = ({
                   value={articleTitle}
                   onChange={() => handleChangeArticleTitle(titleRef.current)}
                 />
-                {/*<InfoTitle>*/}
-                {/*  { detailContent.title }*/}
-                {/*</InfoTitle>*/}
-                {/*<ArticleInfo>*/}
-                {/*  <CreatedTime dateTime="2001-05-15T19:00">2020-12-02 17:04</CreatedTime>*/}
-                {/*  <InfoSubtitle>字數 3,184</InfoSubtitle>*/}
-                {/*  <InfoSubtitle>閱讀人數 49,325</InfoSubtitle>*/}
-                {/*</ArticleInfo>*/}
-                {/*<ArticleContent*/}
-                {/*  dangerouslySetInnerHTML={{ __html: detailContent.content }}*/}
-                {/*>*/}
-                {/*</ArticleContent>*/}
-
-              </GridWhiteBackground>
-
-              <GridWhiteBackground item xs={12}>
                 <CKEditor
-                  editor={ ClassicEditor }
-                  onReady={ editor => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log( 'Editor is ready to use!', editor );
-                  } }
+                  editor={ClassicEditor}
                   onChange={handleChangeArticleContent}
                   data={articleContent}
                 />
+                <SubmitPostButton
+                  onClick={() => {
+                    handleSubmitPost(articleTitle, articleContent)
+                    getMyPostList()
+                  }}
+                >發布文章</SubmitPostButton>
               </GridWhiteBackground>
-              <button
-                onClick={() => handleSubmitPost(articleTitle, articleContent)}
-              >Post</button>
+
+              <GridWhiteBackground item xs={12} style={{marginTop: '4rem'}}>
+                <SectionTitle>我的文章</SectionTitle>
+                <PostList>
+                  { postList.length > 0 && renderMyPostList(postList) }
+                </PostList>
+              </GridWhiteBackground>
             </Grid>
           </WriteWrapper>
         </WritePage>
-
-      // )
+      )
   )
 }
 
@@ -88,6 +98,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = {
+  keepUserLogin: actions.keepUserLogin,
   getMyPostList: actions.getMyPostList,
   handleChangeArticleTitle: actions.handleChangeArticleTitle,
   handleChangeArticleContent: actions.handleChangeArticleContent,
