7bb09f4de76e80ac67b0251f110c7a4c44da6208
= <=>
2021-03-20 13:33:29 +0800

    chore: clean file location

--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,19 +1,19 @@
 import { combineReducers } from 'redux'
 import { reducers as navbarReducer } from 'components/Header/Navbar/stores'
 import { reducers as recommendListReducer } from 'components/Header/Navbar/SearchRecommend/stores'
-import { reducers as homeReducer } from '../../views/Home/stores'
-import { reducers as listReducer } from '../../views/Home/List/stores'
-import { reducers as detailReducer } from '../../views/Detail/stores'
-import { reducers as loginReducer } from '../../views/Login/stores'
-import { reducers as downloadReducer } from '../../views/Download/stores'
-import { reducers as writeReducer } from '../../views/Write/stores'
+import { reducers as homeReducer } from 'views/Home/stores'
+import { reducers as listReducer } from 'views/Home/List/stores'
+import { reducers as detailReducer } from 'views/Detail/stores'
+import { reducers as loginReducer } from 'views/Login/stores'
+import { reducers as downloadReducer } from 'views/Download/stores'
+import { reducers as writeReducer } from 'views/Write/stores'
 
 const reducer = combineReducers({
   navbar: navbarReducer,
   recommend: recommendListReducer,
   home: homeReducer,
   list: listReducer,
-  detailContent: detailReducer,
+  detail: detailReducer,
   login: loginReducer,
   download: downloadReducer,
   write: writeReducer
