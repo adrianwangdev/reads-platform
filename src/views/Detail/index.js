a956984f4ac938c85463a773e48dda4b8c2b9e20
= <=>
2021-03-20 13:38:08 +0800

    feat: build detail page

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -1,54 +1,103 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { actions } from './stores'
-import { Grid } from '@material-ui/core'
+import React, {useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {actions} from './stores'
+import {Grid} from '@material-ui/core'
 
 /* Styles */
 import {
+  ArticleContent,
+  ArticleInfo,
+  AuthorAvatar,
+  AuthorDescription,
+  AuthorFollowButton,
+  AuthorInfo,
+  AuthorName,
+  AuthorWrapper,
   DetailPage,
   DetailWrapper,
   GridWhiteBackground,
-  ArticleInfo,
-  InfoTitle,
-  CreatedTime,
   InfoSubtitle,
-  ArticleContent
+  InfoTitle,
+  MoreArticleLikeAmount,
+  MoreArticleTitle,
+  MoreArticleWrapper,
+  SectionTitle
 } from './detail.styled'
 
 const Detail = ({
-  detailContent,
+  articleDetail,
+  articleList,
   getDetail
 }) => {
 
   const { id } = useParams()
 
+  const renderArticleDetail = data => (
+    <>
+      <InfoTitle>{data.title}</InfoTitle>
+      <ArticleInfo>
+        <InfoSubtitle>{data.like} 位讀者喜歡這篇文章</InfoSubtitle>
+      </ArticleInfo>
+      <ArticleContent dangerouslySetInnerHTML={{__html: data.content}} />
+    </>
+  )
+
+  const renderAuthor = data => (
+    <AuthorWrapper>
+      <AuthorAvatar>
+        <img src={data.authorAvatar} alt=""/>
+      </AuthorAvatar>
+      <AuthorInfo>
+        <AuthorName>{data.authorName}</AuthorName>
+        <AuthorDescription>
+          <p>文章<span>28</span></p>
+          <p>追蹤<span>13.4k</span></p>
+        </AuthorDescription>
+      </AuthorInfo>
+      <AuthorFollowButton>追蹤</AuthorFollowButton>
+    </AuthorWrapper>
+  )
+
+  const renderMoreArticle = () => (
+    <>
+      <SectionTitle>更多 {articleDetail.authorName} 的文章</SectionTitle>
+      <MoreArticleWrapper>
+        <MoreArticleTitle>其它文章 1</MoreArticleTitle>
+        <MoreArticleLikeAmount>49 位讀者喜歡這篇文章</MoreArticleLikeAmount>
+      </MoreArticleWrapper>
+      <MoreArticleWrapper>
+        <MoreArticleTitle>其它文章 2</MoreArticleTitle>
+        <MoreArticleLikeAmount>49 位讀者喜歡這篇文章</MoreArticleLikeAmount>
+      </MoreArticleWrapper>
+      <MoreArticleWrapper>
+        <MoreArticleTitle>其它文章 3</MoreArticleTitle>
+        <MoreArticleLikeAmount>49 位讀者喜歡這篇文章</MoreArticleLikeAmount>
+      </MoreArticleWrapper>
+    </>
+  )
+
   useEffect(() => {
-    getDetail(id)
+    getDetail(id, articleList)
   }, [getDetail, id])
 
   return (
-      <DetailPage>
+    <DetailPage>
       <DetailWrapper maxWidth="md">
-        <Grid container spacing={4}>
+        <Grid container spacing={4} alignItems={'flex-start'}>
           <GridWhiteBackground item xs={8} className="mr16">
-            <InfoTitle>
-              { detailContent.title }
-            </InfoTitle>
-            <ArticleInfo>
-              <CreatedTime dateTime="2001-05-15T19:00">2020-12-02 17:04</CreatedTime>
-              <InfoSubtitle>字數 3,184</InfoSubtitle>
-              <InfoSubtitle>閱讀人數 49,325</InfoSubtitle>
-            </ArticleInfo>
-            <ArticleContent
-              dangerouslySetInnerHTML={{ __html: detailContent.content }}
-            >
-            </ArticleContent>
+            { renderArticleDetail(articleDetail) }
           </GridWhiteBackground>
-          <GridWhiteBackground item xs={4}>
-            作者資訊
-            推薦閱讀
-          </GridWhiteBackground>
+          <Grid item xs={4}>
+            <Grid container spacing={4}>
+              <GridWhiteBackground item xs={12} className="mb16">
+                { renderAuthor(articleDetail) }
+              </GridWhiteBackground>
+              <GridWhiteBackground item xs={12}>
+                { renderMoreArticle() }
+              </GridWhiteBackground>
+            </Grid>
+          </Grid>
         </Grid>
       </DetailWrapper>
     </DetailPage>
@@ -56,7 +105,8 @@ const Detail = ({
 }
 
 const mapStateToProps = state => ({
-  detailContent: state.detailContent
+  articleDetail: state.detail.articleDetail,
+  articleList: state.list.articleList
 })
 
 const mapDispatchToProps = {
